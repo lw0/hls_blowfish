9c6e44865d30b1c87b2cbbd25a8b53b639c446bc
Lukas Wenzel <lukas.wenzel@student.hpi.de>
2017-06-12 18:36:51 +0200

    fix hardware compilation errors

--- hardware/hls_blowfish.cpp
+++ hardware/hls_blowfish.cpp
@@ -10,93 +10,97 @@
 bf_P_t g_P;
 bf_S_t g_S;
 
-bf_uiHalfBlock_t bf_f(bf_uiHalfBlock_t h)
+bf_halfBlock_t bf_f(bf_halfBlock_t h)
 {
-    bf_uiSEntry_t a = (bf_uiSEntry_t)(h >> 24),
-                  b = (bf_uiSEntry_t)(h >> 16),
-                  c = (bf_uiSEntry_t)(h >> 8),
-                  d = (bf_uiSEntry_t) h;
+    bf_sEntry_t a = (bf_sEntry_t)(h >> 24),
+                b = (bf_sEntry_t)(h >> 16),
+                c = (bf_sEntry_t)(h >> 8),
+                d = (bf_sEntry_t) h;
     return ((g_S[0][a] + g_S[1][b]) ^ g_S[2][c]) + g_S[3][d];
 }
 
-bf_block_t encrypt(bf_block_t block)
+void bf_encrypt(bf_halfBlock_t & left, bf_halfBlock_t & right)
 {
-    bf_block_t b = block;
     for (int i = 0; i < 16; i += 2)
     {
-        b.left ^= g_P[i];
-        b.right ^= bf_f(b.left);
-        b.right ^= g_P[i+1];
-        b.left ^= bf_f(b.right);
+        left ^= g_P[i];
+        right ^= bf_f(left);
+        right ^= g_P[i+1];
+        left ^= bf_f(right);
     }
-    b.left ^= g_P[16];
-    b.right ^= g_P[17];
+    left ^= g_P[16];
+    right ^= g_P[17];
 
     // swap left, right
-    bf_uiHalfBlock_t tmp = b.left;
-    b.left = b.right;
-    b.right = tmp;
+    bf_halfBlock_t tmp = left;
+    left = right;
+    right = tmp;
 }
 
-bf_block_t decrypt(const bf_block_t block)
+void bf_decrypt(bf_halfBlock_t & left, bf_halfBlock_t & right)
 {
-    bf_block_t b = block;
     for (int i = 16; i > 0; i -= 2)
     {
-        b.left ^= P[i+1];
-        b.right ^= bf_f(S, b.left);
-        b.right ^= P[i];
-        b.left ^= bf_f(S, b.right);
+        left ^= g_P[i+1];
+        right ^= bf_f(left);
+        right ^= g_P[i];
+        left ^= bf_f(right);
     }
-    b.left ^= P[1];
-    b.right ^= P[0];
+    left ^= g_P[1];
+    right ^= g_P[0];
 
     // swap left, right
-    bf_uiHalfBlock_t tmp = b.left;
-    b.left = b.right;
-    b.right = tmp;
+    bf_halfBlock_t tmp = left;
+    left = right;
+    right = tmp;
 }
 
-bool bf_keyInit(bf_uiHalfBlock_t * key, snapu8_t keyWords)
+bool bf_keyInit(bf_halfBlock_t * key, snapu8_t keyWords)
 {
-    g_P = c_initP;
-    g_S = c_initS;
     for (int i = 0; i < 18; ++i)
     {
-        g_P ^= key[i%keyWords];
+        g_P[i] = c_initP[i] ^ key[i%keyWords];
     }
-    bf_blockStruct_t b = {.all = 0};
+    for (int n=0 ; n<4 ; ++n)
+    {
+        for (int i=0 ; i<256; ++i)
+        {
+            g_S[n][i] = c_initS[n][i];
+        }
+    }
+    bf_halfBlock_t left = 0, right = 0;
     for (int i = 0; i < 18; i += 2)
     {
-        b = encrypt(b);
-        g_P[i] = b.left;
-        g_P[i+1] = b.right;
+        bf_encrypt(left, right);
+        g_P[i] = left;
+        g_P[i+1] = right;
     }
     for (int n=0 ; n<4 ; ++n)
     {
         for (int i=0 ; i<256; i+=2)
         {
-            b = encrypt(b);
-            g_S[n][i] = b.left;
-            g_S[n][i+1] = b.right;
+            bf_encrypt(left, right);
+            g_S[n][i] = left;
+            g_S[n][i+1] = right;
         }
     }
 }
 
 snapu32_t action_setkey(snap_membus_t * hostMem_in, snapu64_t keyAddr, snapu32_t keyBytes)
 {
-    snapu8_t keyWords = keyBytes >> BF_KEYW_BADR_BITS;
-    if (keyBytes & BF_KEYW_BADR_MASK != 0 || // check keyword (half blockwidth) alignment
-        keyWords < BF_KEYW_MIN || keyWords > BF_KEYW_MAX) // check keyword count
+    snapu64_t keyLineAddr = keyAddr >> ADDR_RIGHT_SHIFT;
+    snapu8_t keyWords = keyBytes >> BF_HBLOCK_BADR_BITS;
+    if ((keyBytes & BF_HBLOCK_BADR_MASK) != 0 || // check keyword (half blockwidth) alignment
+        keyWords < BF_KEY_HBLOCK_MIN || keyWords > BF_KEY_HBLOCK_MAX) // check keyword count
     {
         return SNAP_RETC_FAILURE;
     }
     
     // key fits in a single line, fetch it
-    snap_membus_t keyLine = *(hostMem_in + (keyAddr >> ADDR_RIGHT_SHIFT));
+    snap_membus_t keyLine = *(hostMem_in + keyLineAddr);
     
     // cast line to keyword granularity, initialize g_S, and g_P arrays
-    bf_keyInit((bf_uiHalfBlock_t *)(&keyLine), keyWords);
+    bf_keyInit((bf_halfBlock_t *)(&keyLine), keyWords);
 
     return SNAP_RETC_SUCCESS;
 }
@@ -108,7 +112,7 @@ snapu32_t action_endecrypt(snap_membus_t * hostMem_in, snapu64_t inAddr,
     snapu64_t inLineAddr = inAddr >> ADDR_RIGHT_SHIFT;
     snapu64_t outLineAddr = outAddr >> ADDR_RIGHT_SHIFT;
     snapu32_t dataBlocks = dataBytes >> BF_BLOCK_BADR_BITS;
-    if (dataBlocks & BF_BLOCK_BADR_MASK != 0) // check blockwidth alignment
+    if ((dataBlocks & BF_BLOCK_BADR_MASK) != 0) // check blockwidth alignment
     {
         return SNAP_RETC_FAILURE;
     }
@@ -129,18 +133,18 @@ snapu32_t action_endecrypt(snap_membus_t * hostMem_in, snapu64_t inAddr,
         // blockwise processing
         for (snapu8_t blockOffset = 0; blockOffset < blockCount; ++blockOffset)
         {
-            bf_block_t block = {.all = line(blockOffset, blockOffset+BF_BLOCKBITS)};
+            bf_halfBlock_t left = line(blockOffset+BF_HBLOCKBITS, blockOffset + BF_BLOCKBITS-1);
+            bf_halfBlock_t right = line(blockOffset, blockOffset + BF_HBLOCKBITS-1);
+
             if (decrypt)
-            {
-                block = bf_decrypt(block);
-            }
+                bf_decrypt(left, right);
             else
-            {
-                block = bf_encrypt(block);
-            }
-            line(blockOffset, blockOffset+BF_BLOCKBITS) = block.all;
+                bf_encrypt(left, right);
+
+            line(blockOffset+BF_HBLOCKBITS, blockOffset + BF_BLOCKBITS-1) = left;
+            line(blockOffset, blockOffset + BF_HBLOCKBITS-1) = right;
         }
-        
+
         // write processed line
         *(hostMem_out + outLineAddr + lineOffset) = line;
     }
@@ -172,15 +176,15 @@ void hls_action(snap_membus_t  *din_gmem, snap_membus_t  *dout_gmem,
     // VARIABLES
     snapu32_t ReturnCode;
 
-    snapu64_t input_address;
-    snapu64_t output_address;
-    snapu32_t data_length;
+    snapu64_t inAddr;
+    snapu64_t outAddr;
+    snapu32_t byteCount;
     snapu32_t mode;
 
     /* Required Action Type Detection */
     switch (action_reg->Control.flags) {
         case 0:
-            Action_Config->action_type = (snapu32_t)BFS_ACTION_TYPE;
+            Action_Config->action_type = (snapu32_t)BLOWFISH_ACTION_TYPE;
             Action_Config->release_level = (snapu32_t)HW_RELEASE_LEVEL;
             action_reg->Control.Retc = (snapu32_t)0xe00f;
             return;
@@ -192,22 +196,27 @@ void hls_action(snap_membus_t  *din_gmem, snap_membus_t  *dout_gmem,
     //==================================
 
     // byte address received need to be aligned with port width
-    inAddr  = action_reg->Data.input_data;
-    outAddr  = action_reg->Data.output_data;
-    dataBytes = action_reg->Data.data_length;
+    inAddr  = action_reg->Data.input_data.addr;
+    outAddr  = action_reg->Data.output_data.addr;
+    byteCount = action_reg->Data.data_length;
     mode = action_reg->Data.mode;
 
-    snapu32_t retc = SNAP_RETC_FAILURE;
+    printf("Opcode %d (inAddr: %x, outAddr: %x, byteCount: %d)", mode, inAddr, outAddr, byteCount);
+    printf("Opcodes: SET_KEY %d, ENCRYPT %d, DECRYPT %d)", MODE_SET_KEY, MODE_ENCRYPT, MODE_DECRYPT);
+
+    snapu32_t retc = SNAP_RETC_SUCCESS;
     switch (mode)
     {
-    BF_MODE_SETKEY:
-        retc = action_setkey(din_gmem, inAddr, dataBytes);
+    MODE_SET_KEY:
+        retc = action_setkey(din_gmem, inAddr, byteCount);
         break;
-    BF_MODE_ENCRYPT:
-        retc = action_endecrypt(din_gmem, inAddr, dout_gmem, outAddr, dataBytes, 0);
+    MODE_ENCRYPT:
+        retc = action_endecrypt(din_gmem, inAddr, dout_gmem, outAddr, byteCount, 0);
         break;
-    BF_MODE_DECRYPT:
-        retc = action_endecrypt(din_gmem, inAddr, dout_gmem, outAddr, dataBytes, 1);
+    MODE_DECRYPT:
+        retc = action_endecrypt(din_gmem, inAddr, dout_gmem, outAddr, byteCount, 1);
+        break;
+    default:
         break;
     }
 
