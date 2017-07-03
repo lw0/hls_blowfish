53b8d266b0542c19bd8ec78fa92256cf557edfba
Balthasar Martin <balthasar.martin@student.hpi.de>
2017-07-03 12:15:52 +0200

    extended blowfish test script

--- helpers/blowfish_test.py
+++ helpers/blowfish_test.py
@@ -7,8 +7,9 @@ data = binascii.unhexlify('ffeeddccbbaa99887766554433221100ffeeddccbbaa998877665
 key_reversed = bytes(reversed(key))
 data_reverse = bytes(reversed(data))
 
-for k in [key, key_reversed]:
-	for d in [data, data_reverse]:
-		cipher = blowfish.Cipher(k)
-		data_encrypted = b''.join(cipher.encrypt_ecb(d))
-		print(binascii.hexlify(data_encrypted))
+for endianess in ["little", "big"]:
+	for k in [key, key_reversed]:
+		for d in [data, data_reverse]:
+			cipher = blowfish.Cipher(k, byte_order = "little")
+			data_encrypted = b''.join(cipher.encrypt_ecb(d))
+			print(binascii.hexlify(data_encrypted))
